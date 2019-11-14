f8388af51364a63a185b9fbb65f946511eaaea4a
xkored <xkored@gmail.com>
2019-11-14 18:04:59 +0400

    feat: finish mp

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,33 +1,111 @@
 $(function() {
-  $('.banner_top').owlCarousel({
+  $('.banner').owlCarousel({
       nav: false,
       items: 1,
       loop: false,
       dots: true,
       smartSpeed: 800,
   });
-  // tabs 
-  if ( $(window).width() > 600 ) {
-    $(document).ready(function () {
-      $(".tabs__content-item:not(:first-child)").hide();
-      $(".tabs__container div.tabs__content-item.active-tab").show();
-      $('ul.tabs__list > li').click(function () {
-        if (!($(this).hasClass('active'))) {
-          var thisLi = $(this);
-          var numLi = thisLi.index();
-          thisLi.addClass('active').siblings().removeClass('active');
-          thisLi.parent().next().children('div').hide().eq(numLi).fadeIn('slow');
+  $('.hit__in').owlCarousel({
+      nav: true,
+      loop: false,
+      dots: true,
+      smartSpeed: 800,
+      margin: 30,
+      navText: ["<svg width='11' height='18' viewBox='0 0 11 18' xmlns='http://www.w3.org/2000/svg'><path d='M10 1L2 9L10 17' stroke-width='2'/></svg>","<svg width='11' height='18' viewBox='0 0 11 18' xmlns='http://www.w3.org/2000/svg'><path d='M1 17L9 9L1 0.999999' stroke-width='2'/></svg>"],
+      responsive : {
+        0   : {
+            items: 1
+        },
+        380 : {
+            items: 1
+        },
+        480 : {
+            items: 1
+        },
+        600 : {
+            items: 2
+        },
+        900 : {
+            items: 3
+        },
+        1200 : {
+            items: 4
         }
-      });
-    });
-  };
-  if ( $(window).width() < 600 ) {
-    $('.tabs-acc__btn').on('click', function(){
-      $(this).toggleClass('active').siblings('.tabs-acc__cnt').slideToggle();
-    })
-  };
-  //Закрываем AjaxForm popup после успешной отправки
-  // $(document).on('af_complete', function(event,res) {
-  //   if(res.success) parent.$.fancybox.close();
-  // });
+      }
+  });
+  $('.brand__in').owlCarousel({
+      nav: true,
+      loop: false,
+      dots: false,
+      smartSpeed: 800,
+      margin: 30,
+      navText: ["<svg width='11' height='18' viewBox='0 0 11 18' xmlns='http://www.w3.org/2000/svg'><path d='M10 1L2 9L10 17' stroke-width='2'/></svg>","<svg width='11' height='18' viewBox='0 0 11 18' xmlns='http://www.w3.org/2000/svg'><path d='M1 17L9 9L1 0.999999' stroke-width='2'/></svg>"],
+      responsive : {
+        0   : {
+            items: 1
+        },
+        380 : {
+            items: 1
+        },
+        480 : {
+            items: 2
+        },
+        600 : {
+            items: 3
+        },
+        900 : {
+            items: 4
+        },
+        1200 : {
+            items: 6
+        }
+      }
+  });
+  $('.news__in').owlCarousel({
+      nav: true,
+      loop: false,
+      dots: false,
+      smartSpeed: 800,
+      margin: 30,
+      navText: ["<svg width='11' height='18' viewBox='0 0 11 18' xmlns='http://www.w3.org/2000/svg'><path d='M10 1L2 9L10 17' stroke-width='2'/></svg>","<svg width='11' height='18' viewBox='0 0 11 18' xmlns='http://www.w3.org/2000/svg'><path d='M1 17L9 9L1 0.999999' stroke-width='2'/></svg>"],
+      responsive : {
+        0   : {
+            items: 1
+        },
+        380 : {
+            items: 1
+        },
+        600 : {
+            items: 1
+        },
+        900 : {
+            items: 2
+        },
+        1200 : {
+            items: 3
+        }
+      }
+  });
+  // tabs 
+  // if ( $(window).width() > 600 ) {
+  //   $(document).ready(function () {
+  //     $(".tabs__content-item:not(:first-child)").hide();
+  //     $(".tabs__container div.tabs__content-item.active-tab").show();
+  //     $('ul.tabs__list > li').click(function () {
+  //       if (!($(this).hasClass('active'))) {
+  //         var thisLi = $(this);
+  //         var numLi = thisLi.index();
+  //         thisLi.addClass('active').siblings().removeClass('active');
+  //         thisLi.parent().next().children('div').hide().eq(numLi).fadeIn('slow');
+  //       }
+  //     });
+  //   });
+  // };
+  // if ( $(window).width() < 600 ) {
+  //   $('.tabs-acc__btn').on('click', function(){
+  //     $(this).toggleClass('active').siblings('.tabs-acc__cnt').slideToggle();
+  //   })
+  // };
+
 });
